9596539ece25b442b4ebbf49011aae32dca24441
tenth.man <tenth.man@mail.ru>
2024-07-25 18:52:29 +0300

    first-travel

---
+++ scripts/script.js
@@ -0,0 +1,68 @@
+/*var link = document.querySelector('link[rel=import]');
+var header = link.import.querySelector("#head");
+let nextElem = document.querySelector('article');
+document.body.insertBefore(header, nextElem);
+console.log(nextElem);*/
+
+
+/*let nameOfPage;
+let headerElement = document.createElement('header');
+let divElementNav = document.createElement('div');
+let spanElement = document.createElement("span");
+let redDotElement = document.createElement('b');
+let ulElement = document.createElement('ul');
+let liElement1 = document.createElement('li');
+let liElement2 = document.createElement('li');
+let liElement3 = document.createElement('li');
+let liElement4 = document.createElement('li');
+let aElement1 = document.createElement('a');
+let aElement2 = document.createElement('a');
+let aElement3 = document.createElement('a');
+let aElement4 = document.createElement('a');
+
+let bodyElement = document.querySelector('body');
+let divMainElement = document.querySelector('.main');
+
+bodyElement.insertBefore(headerElement, divMainElement);
+
+divElementNav.setAttribute("class", "nav");
+divElementNav.appendChild(ulElement);
+
+ulElement.appendChild(liElement1);
+ulElement.appendChild(liElement2);
+ulElement.appendChild(liElement3);
+ulElement.appendChild(liElement4);
+
+liElement1.appendChild(aElement1);
+aElement1.setAttribute("href", "index.html");
+aElement1.setAttribute("class", "active");
+
+aElement1.textContent = "Главная";
+
+liElement2.appendChild(aElement2);
+aElement2.setAttribute("href", "Albums.html");
+
+aElement2.textContent = "Альбомы";
+
+liElement3.appendChild(aElement3);
+aElement3.setAttribute("href", "Tracks.html");
+
+aElement3.textContent = "Маршруты";
+
+liElement4.appendChild(aElement4);
+aElement4.setAttribute("href", "Contacts.html");
+
+aElement4.textContent = "Кто мы";
+
+headerElement.appendChild(divElementNav);
+
+spanElement.setAttribute("class", "logo");
+headerElement.insertBefore(spanElement, divElementNav);
+spanElement.textContent = document.querySelector('script').dataset.nameOfPage;
+
+redDotElement.textContent = ".";
+spanElement.appendChild(redDotElement);*/
+
+//let templateHdr = document.querySelector("#hdr");
+//let clone = templateHdr.content.cloneNode(true);
+//templateHdr.parentNode.appendChild(clone);
